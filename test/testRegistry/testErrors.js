294780453ab8dde093305b1c61a346eaf32e11a2
intertext.no <orstavik77@gmail.com>
2020-08-20 11:56:28 +0200

    fixing errors in adding last/first event listeners wrongly

--- test/testRegistry/testErrors.js
+++ test/testRegistry/testErrors.js
@@ -1,17 +1,4 @@
 export const errorsTest = [{
-  name: "First error: adding two first listeners",
-  fun: function (res) {
-    const h1 = document.createElement("h1");
-    h1.addEventListener("click", e => res.push("a"), {first: true, capture: true});
-    try {
-      h1.addEventListener("click", e => res.push("b"), {first: true, capture: true});
-    } catch (e) {
-      res.push("error ");
-    }
-    h1.dispatchEvent(new MouseEvent("click"));
-  },
-  expect: "error a"
-}, {
   name: "First error: first on bubble phase",
   fun: function (res) {
     const h1 = document.createElement("h1");
@@ -28,19 +15,6 @@ export const errorsTest = [{
     h1.dispatchEvent(new MouseEvent("click"));
   },
   expect: "12"
-}, {
-  name: "Last error: adding two last listeners",
-  fun: function (res) {
-    const h1 = document.createElement("h1");
-    h1.addEventListener("click", e => res.push("a"), {last: true});
-    try {
-      h1.addEventListener("click", e => res.push("b"), {last: true});
-    } catch (e) {
-      res.push("1");
-    }
-    h1.dispatchEvent(new MouseEvent("click"));
-  },
-  expect: "1a",
 }, {
   name: "Last error: capture phase listener ",
   fun: function (res) {
@@ -65,4 +39,30 @@ export const errorsTest = [{
     h1.dispatchEvent(new Event("click"));
   },
   expect: "error"
+}, {
+  name: "Last error: adding two last listeners",
+  fun: function (res) {
+    const h1 = document.createElement("h1");
+    h1.addEventListener("click", e => res.push("a"), {last: true});
+    try {
+      h1.addEventListener("click", e => res.push("b"), {last: true});
+    } catch (e) {
+      res.push("1");
+    }
+    h1.dispatchEvent(new MouseEvent("click"));
+  },
+  expect: "1a",
+}, {
+  name: "First error: adding two first listeners",
+  fun: function (res) {
+    const h1 = document.createElement("h1");
+    h1.addEventListener("click", e => res.push("a"), {first: true, capture: true});
+    try {
+      h1.addEventListener("click", e => res.push("b"), {first: true, capture: true});
+    } catch (e) {
+      res.push("error ");
+    }
+    h1.dispatchEvent(new MouseEvent("click"));
+  },
+  expect: "error a"
 }];
